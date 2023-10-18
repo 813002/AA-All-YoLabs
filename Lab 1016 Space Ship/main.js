d38651ba31d174271224bd27f9286916f81d554e
Yoan Renninger <146980588+813002@users.noreply.github.com>
2023-10-18 07:48:47 -0700

    Began working on planet class/prototype

--- Lab 1016 Space Ship/main.js
+++ Lab 1016 Space Ship/main.js
@@ -4,12 +4,12 @@ let ship, planet;
 function init(){ //setup
     canvas = document.getElementById("cnv");
     resizeCanvas(canvas);
-    context = canvas.getContext("2d");   
-    
+    context = canvas.getContext("2d");
+
     animate();
 }
 
-function animate() { //draw  
+function animate() { //draw
     context.clearRect(0,0,canvas.width,canvas.height);
     context.fillStyle = "rgba(100,100,200,1)";
     context.fillRect(0, 0, canvas.width, canvas.height)
@@ -17,7 +17,10 @@ function animate() { //draw
     requestAnimationFrame(animate);
 
 
-    ship = new Ship(40, 40)
+    ship = new Ship(40, 40);
+    ship.run();
+
+    planet = new Planet
 }
 
 
--- Lab 1016 Space Ship/planet.js
+++ Lab 1016 Space Ship/planet.js
@@ -1,5 +1,5 @@
-function Planet(){
-    this.loc = new JSVector();
+function Planet(x, y){
+    this.loc = new JSVector(x, y);
 }
 
 Planet.prototype.run = function(){
@@ -8,9 +8,16 @@ Planet.prototype.run = function(){
 }
 
 Planet.prototype.render = function(){
-
+  context.strokeStyle = this.col;
+  context.fillStyle = this.col;
+  context.beginPath();
+  context.arc(this.loc.x, this.loc.y, this.r, Math.PI * 2, 0);
+  context.stroke();
+  context.fill();
 }
 
 Planet.prototype.update = function(){
-
+  if(ship.loc.x < this.loc.x){
+    this.loc.x += 200;
+  }
 }
