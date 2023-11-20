3d467c2238bff6f79dee38f39d687e083edb46aa
Yoan Renninger <146980588+813002@users.noreply.github.com>
2023-11-20 09:53:23 -0800

    Created Flocking lab

--- Lab 1116 Flocking/main.js
+++ Lab 1116 Flocking/main.js
@@ -1,29 +1,22 @@
-window.addEventListener("load", init);
 
+//  Eric Ettlin
+//  231120
+//  Flocking Lab
 
-function init(){ //setup
-    let cnv = createCanvas(900, 700);
-    cnv.position((windowWidth - width) /2, 50)
-    
-    // canvas = document.getElementById("cnv");
-    // context = canvas.getContext("2d"); 
-    
-    let v1 = new JSVector(3, 4);
-    let v2 = new JSVector(3, 7);
-   
-    v1.add(v2);
-    console.log(v1.x)
+let game;   // a single global object
 
-    // console.log("V1 mag = " + v1.getMagnitude())
-    
-    animate();         
+window.onload = init;//  After the window has been loaded, go to init
+
+function init(){
+    game = new Game();  // global game
+    animate();          // kick off the animation
 }
 
-function animate() { //draw  
-    // erase the HTMLCanvasElement
-    context.clearRect(0,0,canvas.width,canvas.height);
-    requestAnimationFrame(animate); // next cycle
-
-
+//  animation loop called 60 fps
+function animate(){
+    // paint the canvas with mostly transparent black
+  game.ctx.fillStyle = 'rgba(0,0,0,.5)'
+  game.ctx.fillRect(0,0,game.canvas.width,game.canvas.height);
+  game.run();    // run the game
+  requestAnimationFrame(animate);
 }
-
